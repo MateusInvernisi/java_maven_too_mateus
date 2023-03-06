40c8f436c4c6e04208bcf6b6de0174fd0b94fd81
mateu <mateu@10.5.89.58>
2023-03-06 11:49:30 -0300

    .

---
+++ src/main/java/br/edu/ifsul/bcc/too/Teste.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.bcc.too;
+
+/**
+ *
+ * @author mateu
+ */
+public class Teste {
+    public static void main(String args[]){
+        
+        System.out.println("Teste 123");
+        
+        /*1. Definir um métoo que receba por parametros dois valores inteiros,
+        esse método deverá calcular a média do valores recebidos e reotrnar 
+        um valor float
+        
+        2. criar o método main, chamar o método criado no exer1, informar o 
+        
+        */
+    }
+}
+
